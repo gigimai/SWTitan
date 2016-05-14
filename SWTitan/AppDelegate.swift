4c28a7c1eed39d2a0686c3653992065c50c22758
Mai mai <gigi10991@gmail.com>
2016-05-14 13:29:50 +0700

    4 clean up

--- SWTitan/AppDelegate.swift
+++ SWTitan/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         UIApplication.sharedApplication().statusBarHidden = false
-        //Register to access location
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window?.backgroundColor = UIColor.whiteColor()
         let splashViewController = SplashViewController()
